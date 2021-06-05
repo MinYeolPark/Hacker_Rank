ba95dca8199061179bed826622103a817e5b9882
MinyeolPark <pmy30813@gmail.com>
2021-06-05 16:51:02 +0900

    Create StringStream_HackerRank.cpp
    Hacker_Rank solution

---
+++ StringStream_HackerRank.cpp
@@ -0,0 +1,33 @@
+﻿#include <sstream>
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+vector<int> parseInts(string str)
+{
+	vector<int> vect_ret;
+	stringstream ss(str);
+	ss.str(str);
+
+	for (int i = 0; ss >> i; ss.ignore())
+	{
+		vect_ret.push_back(i);
+	}
+
+	return vect_ret;
+}
+int main()
+{
+	string str;
+	cin >> str;
+	vector<int>	integers = parseInts(str);
+	for (int i = 0; i < integers.size(); i++)
+	{
+		cout << integers[i] << "\n";
+	}
+
+
+	return 0;
+}
