44e3775367b2e94eec795c1c9044c0cdfdb27ccb
박민열 <67354549+MinYeolPark@users.noreply.github.com>
2021-06-30 13:58:19 +0900

    Create Vector_Erase.cpp
    Hackerrank_solution

---
+++ Vector_Erase.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	vector<int> v;
+	int n,x;
+	cin >> n;
+
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> x;
+		v.push_back(x);
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i]<<" ";
+	}
+	cout << endl;
+	
+	int a, b, c;
+	cin >> a;
+	
+	v.erase(v.begin() + a - 1);
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+	cin >> b >> c;
+	v.erase(v.begin() + b - 1, v.begin() + c - 1);
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+
+	cout << v.size() << endl;
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " " ;
+	}
+
+}
